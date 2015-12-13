0ba937adf8c4a072155df25e7969d7ec7922b59d
deadking01 <wuqingfeng234@163.com>
2015-12-13 23:30:26 +0800

    logger generator init

---
+++ loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/App.java
@@ -0,0 +1,13 @@
+package com.huawei.wuqf.loggergenerator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
