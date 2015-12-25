851fa32a90ee043821aaeb3bf296ba21b65fc21a
deadking01 <wuqingfeng234@163.com>
2015-12-25 21:02:54 +0800

    begin~~

---
+++ loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/FlumeAppender.java
@@ -0,0 +1,28 @@
+package com.huawei.wuqf.loggergenerator;
+
+import org.apache.log4j.AppenderSkeleton;
+import org.apache.log4j.spi.LoggingEvent;
+
+public class FlumeAppender extends AppenderSkeleton
+{
+
+    public void close()
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+    public boolean requiresLayout()
+    {
+        // TODO Auto-generated method stub
+        return false;
+    }
+
+    @Override
+    protected void append(LoggingEvent arg0)
+    {
+        // TODO Auto-generated method stub
+        
+    }
+
+}
