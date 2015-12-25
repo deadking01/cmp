b2690611a4c5c699501000d6804cc387d09da302
deadking01 <wuqingfeng234@163.com>
2015-12-25 21:15:05 +0800

    debug

--- loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/App.java
+++ loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/App.java
@@ -1,13 +1,38 @@
 package com.huawei.wuqf.loggergenerator;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App
 {
-    public static void main( String[] args )
+    protected static final Logger logger = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args)
     {
-        System.out.println( "Hello World!" );
+        for (int i = 0; i < 10; i++)
+        {
+            new Thread(new Runnable()
+            {
+                public void run()
+                {
+                    for (int j = 0; j < 100; j++)
+                    {
+                        logger.debug("i is "+j+" . j is "+j);
+                        try
+                        {
+                            Thread.sleep(1000);
+                        }
+                        catch (InterruptedException e)
+                        {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            }).start();
+        }
     }
 }
--- loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/FlumeAppender.java
+++ loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/FlumeAppender.java
@@ -9,20 +9,16 @@ public class FlumeAppender extends AppenderSkeleton
     public void close()
     {
         // TODO Auto-generated method stub
-        
     }
 
     public boolean requiresLayout()
     {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     protected void append(LoggingEvent arg0)
     {
-        // TODO Auto-generated method stub
-        
     }
 
 }
