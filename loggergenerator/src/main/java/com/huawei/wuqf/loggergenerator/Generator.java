5b8540403836fb47ba1658590c05ec0f65512271
deadking01 <wuqingfeng234@163.com>
2015-12-30 23:06:58 +0800

    update

--- loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/Generator.java
+++ loggergenerator/src/main/java/com/huawei/wuqf/loggergenerator/Generator.java
@@ -7,9 +7,9 @@ import org.slf4j.LoggerFactory;
  * Hello world!
  *
  */
-public class Genetator
+public class Generator
 {
-    protected static final Logger logger = LoggerFactory.getLogger(Genetator.class);
+    protected static final Logger logger = LoggerFactory.getLogger(Generator.class);
     
     public static void main(String[] args)
     {
@@ -19,9 +19,9 @@ public class Genetator
             {
                 public void run()
                 {
-                    for (int j = 0; j < 100; j++)
+                    for (int j = 0; j < 100000000; j++)
                     {
-                        logger.debug("i is "+j+" . j is "+j);
+                        logger.debug("j is "+j);
                         try
                         {
                             Thread.sleep(1000);
