61133d2987e7203c92c78428a6d5b1811107617d
wuqf <wuqingfeng234@163.com>
2016-10-11 00:47:13 +0800

    debug spark

--- client/hbase-spark/src/test/java/com/huawei/wuqf/AppTest.java
+++ client/hbase-spark/src/test/java/com/huawei/wuqf/AppTest.java
@@ -16,12 +16,12 @@ public class AppTest
 
 {
     public static void main(String[] args){
-        SparkConf conf = new SparkConf().setAppName("appName").set("spark.executor.memory", "30M").setMaster("spark://wuqf-B85M-D2V:7077");
+        SparkConf conf = new SparkConf().setAppName("appName").set("spark.executor.memory", "300M").setMaster("spark://wuqf-B85M-D2V:7077");
         JavaSparkContext sc = new JavaSparkContext(conf);
         List<Integer> data = Arrays.asList(1, 2, 3, 4, 5);
         JavaRDD<Integer> distData = sc.parallelize(data);
 
-        JavaRDD<String> lines = sc.textFile("pom.xml");
+        JavaRDD<String> lines = sc.textFile("aa");
         JavaRDD<Integer> lineLengths=lines.map(new Function<String, Integer>() {
             @Override
             public Integer call(String s) throws Exception {
